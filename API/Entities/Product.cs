8bb21a508e2fe4e8453a3a6b87d668a99f6f06ca
Abhishek Srivastava <abhishek.srivastava@aquevix.com>
2022-03-25 01:21:01 +0530

    Add AppUser, Product and Categories(Also add some data for prouduct and categories)

---
+++ API/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public Category? Category { get; set; }
+        public int CategoryId { get; set; }
+        public bool Featured { get; set; } = false;
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
+        public bool Visible { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped]
+        public bool Editing { get; set; } = false;
+        [NotMapped]
+        public bool IsNew { get; set; } = false;
+    }
+}
---
+++ API/Entities/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [NotMapped]
+        public bool Editing { get; set; } = false;
+        [NotMapped]
+        public bool IsNew { get; set; } = false;
+    }
+}
